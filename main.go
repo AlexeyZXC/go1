2267596d5026a42847a268c4817b179f36a0d5e9
AlexeyZXC <alexey.sakharov@list.ru>
2022-02-04 07:52:14 +0300

    row and column entering

--- main.go
+++ main.go
@@ -6,9 +6,20 @@ import (
 	"os"
 )
 
+func readRowColumn() (row byte, column byte) {
+	fmt.Println("Enter row and column")
+	fmt.Scan(&row, &column)
+	return row, column
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("What will you play? ")
 	chip, _ := reader.ReadString('\n')
-	fmt.Println("You play with " + chip)
+	fmt.Println("You play with: " + chip)
+
+	// enter row and column
+	var row, column byte
+	row, column = readRowColumn()
+	fmt.Printf("Row: %v; Column: %v ", row, column)
 }
