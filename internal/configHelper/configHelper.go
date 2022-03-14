47f6b8e533601fb3fe00655e8e5a7dcade10f27f
AlekseySakharov <Aleksey.Sakharov@imaginecommunications.com>
2022-03-14 10:27:36 +0300

    lesson-9

--- internal/configHelper/configHelper.go
+++ internal/configHelper/configHelper.go
@@ -1,9 +1,13 @@
 package configHelper
 
 import (
+	"errors"
+	"io/ioutil"
 	"net/url"
 	"os"
 	"strconv"
+
+	"gopkg.in/yaml.v2"
 )
 
 // port: 8080
@@ -12,12 +16,32 @@ import (
 var prefix string
 
 type Config struct {
-	Port   uint
-	Db_url string
+	Port   uint   `yaml:"port"`
+	Db_url string `yaml:"db_url"`
 }
 
 func SetPrefix(value string) {
 	prefix = value
+}
+
+func (c *Config) empty() {
+	c.Db_url = ""
+	c.Port = 0
+}
+
+func (c *Config) validate() (err error) {
+	if c.Port > 65535 {
+		err = errors.New("Invlaid port")
+		c.empty()
+		return
+	}
+
+	if _, err = url.ParseRequestURI(c.Db_url); err != nil {
+		c.empty()
+		return
+	}
+
+	return
 }
 
 func (c *Config) ReadEnvVar() (err error) {
@@ -28,9 +52,10 @@ func (c *Config) ReadEnvVar() (err error) {
 	c.Port = uint(p)
 	c.Db_url = os.Getenv(prefix + "Db_url")
 
-	if _, err := url.ParseRequestURI(c.Db_url); err != nil {
-		return err
+	if err = c.validate(); err != nil {
+		c = &Config{}
 	}
+
 	return
 }
 
@@ -41,4 +66,23 @@ func (c Config) WriteEnvVar() (err error) {
 	}
 	err = os.Setenv(prefix+"db_url", c.Db_url)
 	return err
+}
+
+//--- yaml
+
+func (c *Config) ReadFromYaml() (err error) {
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		return
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		return
+	}
+
+	if err = c.validate(); err != nil {
+		c = nil
+	}
+
+	return
 }
